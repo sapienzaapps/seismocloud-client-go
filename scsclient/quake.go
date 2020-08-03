28fe08c52888c072e4459882f60b2d905a0e4c8d
Enrico204 <enrico204@gmail.com>
2020-08-03 21:07:17 +0200

    Fix bug in quake api call

--- scsclient/quake.go
+++ scsclient/quake.go
@@ -6,7 +6,7 @@ import (
 )
 
 func (c *_clientimpl) Quake(quaketime time.Time, x float64, y float64, z float64) error {
-	token := c.mqttc.Publish(fmt.Sprintf("sensor/%s/streamdata", c.opts.DeviceId), 0, false,
+	token := c.mqttc.Publish(fmt.Sprintf("sensor/%s/quake", c.opts.DeviceId), 0, false,
 		fmt.Sprintf("%d;%f;%f;%f", quaketime.UnixNano()/1000000, x, y, z))
 	token.Wait()
 	return token.Error()
