6ee182fc94fbea061d40c7def56d12b30fb0ef36
Enrico204 <enrico204@gmail.com>
2021-05-16 23:13:49 +0200

    Fix threshold topic name typo

--- scsclient/send-threshold.go
+++ scsclient/send-threshold.go
@@ -6,7 +6,7 @@ import (
 )
 
 func (c *_clientimpl) SendThreshold(threshold float64) error {
-	token := c.mqttc.Publish(fmt.Sprintf("sensor/%s/treshold", c.opts.DeviceID), 0, false, fmt.Sprintf("%f", threshold))
+	token := c.mqttc.Publish(fmt.Sprintf("sensor/%s/threshold", c.opts.DeviceID), 0, false, fmt.Sprintf("%f", threshold))
 	if !token.WaitTimeout(clientTimeout) {
 		return errors.New("command timeout")
 	}
